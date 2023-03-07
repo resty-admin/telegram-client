9a232cd8c082f013215deb36ba83016d81ac5ea1
Viacheslav Saloid <viacheslavsaloid.work@gmail.com>
2023-03-07 09:46:22 +0200

    fix: texts

--- src/app/bot/updates/bot/bot.update.ts
+++ src/app/bot/updates/bot/bot.update.ts
@@ -4,12 +4,19 @@ import { STICKERS } from "src/app/shared/constants";
 import { environment } from "../../../../environments/environment";
 import { IStateContext } from "../../../shared";
 
-const text = `Привіт 👋🏼
+const text1 = `Привіт 👋🏼
 Я - твій онлайн офіціант, Resty!
 
 Хочеш зробити замовлення, забронювати стіл у закладі, чи ти готовий сплатити за свій рахунок?
 
 Я тут для того, щоб зробити це швидко та зручно 🙌🏼`;
+
+const text2 = `Привіт 👋🏼
+Я - твій онлайн офіціант, Resty!
+
+Бачу, ти вже визначився зі столиком
+
+Переходь додаток і я допоможу тобі оформити замовлення`;
 
 @Update()
 export class BotUpdate {
@@ -23,7 +30,7 @@ export class BotUpdate {
 
 		const url = placeId && code ? `/places/${placeId}/connect-to-table?code=${code}&from=telegram` : "?from=telegram";
 
-		await context.reply(text, {
+		await context.reply(code ? text2 : text1, {
 			reply_markup: {
 				inline_keyboard: [[{ text: "Почати", web_app: { url: `${environment.appUrl}${url}` } }]]
 			}
